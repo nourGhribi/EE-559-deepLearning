425a97aec3fb2730c250dde0b9ac8b2aae632737
nourGhribi <nour.ghribi@epfl.ch>
2021-05-23 16:32:48 +0200

    working on proj1

--- Project2/data_generator.py
+++ Project2/data_generator.py
@@ -4,10 +4,10 @@ import torch
 
 def get_data(n):
     """
-    Returns train and test data x,y each having n points sampled uniformly in [0, 1]^2,
+    Returns train and test data x, y each having n points sampled uniformly in [0, 1]^2,
     each with a label 0 if outside the disk centered at (0.5, 0.5) of radius 1/√2π, and 1 if inside.
-    :param n:
-    :return:
+    :param n: Number of data points
+    :return: data, labels
     """
     x = torch.empty(n, 2)
     x = x.uniform_(0, 1)
