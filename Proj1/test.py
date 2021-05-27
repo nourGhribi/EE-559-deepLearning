01b22be3fcc9d880e9b46f13eb3f1ff857924722
salimbenghorbel <selim.benghorbal@gmail.com>
2021-05-27 02:06:06 +0200

    Updated parameter search.

--- Proj1/test.py
+++ Proj1/test.py
@@ -33,9 +33,9 @@ print('test_input:', test_input.size(), 'test_classes:', test_classes.size(), 't
 optimizer_deep = optim.Adam
 nb_epoch_deep = 25
 lr_deep = 0.001
-mini_batch_size_deep = 1
-nb_hidden_deep = 100
-auxiliary_loss_weight_deep = 0.5
+mini_batch_size_deep = 100
+nb_hidden_deep = 300
+auxiliary_loss_weight_deep = 0
 print("Loading pickled DeepModel...")
 deep = DeepModel(nb_hidden_deep, nb_epoch_deep, lr_deep, mini_batch_size_deep, auxiliary_loss_weight_deep).to(device)
 deep.load_state_dict(torch.load('data/pickles/deep/best_model.pt'))
@@ -48,9 +48,9 @@ print("optimizer = {0}, nb_epoch = {1}, lr = {2}, mini_batch_size = {3}, nb_hidd
 optimizer_deep_ws = optim.Adam
 nb_epoch_deep_ws = 25
 lr_deep_ws = 0.001
-mini_batch_size_deep_ws = 1
-nb_hidden_deep_ws = 100
-auxiliary_loss_weight_deep_ws = 0.5
+mini_batch_size_deep_ws = 100
+nb_hidden_deep_ws = 300
+auxiliary_loss_weight_deep_ws = 0
 print("Loading pickled DeepModelWS...")
 deep_ws = DeepModelWS(nb_hidden_deep_ws, nb_epoch_deep_ws, lr_deep_ws, mini_batch_size_deep_ws, auxiliary_loss_weight_deep_ws).to(device)
 deep_ws.load_state_dict(torch.load('data/pickles/deep_ws/best_model.pt'))
@@ -63,13 +63,12 @@ print("optimizer = {0}, nb_epoch = {1}, lr = {2}, mini_batch_size = {3}, nb_hidd
 optimizer_shallow = optim.Adam
 nb_epoch_shallow = 25
 lr_shallow = 0.001
-mini_batch_size_shallow = 1
-nb_hidden_shallow = 100
-auxiliary_loss_weight_shallow = 0.5
+mini_batch_size_shallow = 100
+nb_hidden_shallow = 500
 print("Loading pickled ShallowModel...")
-shallow = ShallowModel(nb_hidden_shallow, nb_epoch_shallow, lr_shallow, mini_batch_size_shallow, auxiliary_loss_weight_shallow).to(device)
+shallow = ShallowModel(nb_hidden_shallow, nb_epoch_shallow, lr_shallow, mini_batch_size_shallow).to(device)
 shallow.load_state_dict(torch.load('data/pickles/shallow/best_model.pt'))
 print("Training ShallowModel...")
 acc_losses, train_accuracy, test_accuracy, train_error, test_error = helpers.train_report(shallow, train_input, train_target, test_input, test_target, mini_batch_size_shallow)
 print("Obtained test accuracy for ShallowModel is {0} and train accuracy is {1}, using the following parameters:".format(test_accuracy, train_accuracy))
-print("optimizer = {0}, nb_epoch = {1}, lr = {2}, mini_batch_size = {3}, nb_hidden = {4}, auxiliary_loss_weight = {5}".format(optimizer_shallow, nb_epoch_shallow, lr_shallow, mini_batch_size_shallow, nb_hidden_shallow, auxiliary_loss_weight_shallow))
+print("optimizer = {0}, nb_epoch = {1}, lr = {2}, mini_batch_size = {3}, nb_hidden = {4}".format(optimizer_shallow, nb_epoch_shallow, lr_shallow, mini_batch_size_shallow, nb_hidden_shallow))
